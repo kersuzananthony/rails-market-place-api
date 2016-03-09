2c0474e3350618fd5e23f10788b8a4452986b5f1
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-03-09 17:00:11 +0800

    Refactors test headers for each request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,4 +61,9 @@ RSpec.configure do |config|
 
   # Include the test request on every controller test file
   config.include Request::JsonHelpers, :type => :controller
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
