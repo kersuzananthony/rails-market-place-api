e50caa4d0f9648528d002c82b1f019cbf9b69161
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-03-16 12:07:11 +0800

    Adds the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
+  # Events
+  before_validation :set_total!
+
   # Association
   belongs_to :user
   has_many :placements
@@ -7,4 +10,8 @@ class Order < ActiveRecord::Base
   # Validation
   validates :user_id, presence: true
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -18,4 +18,17 @@ describe Order do
   it { should belong_to :user }
   it { should have_many :placements }
   it { should have_many(:products).through(:placements) }
+
+  describe '#set_total!' do
+    before(:each) do
+      product_1 = FactoryGirl.create :product, price: 100
+      product_2 = FactoryGirl.create :product, price: 85
+
+      @order = FactoryGirl.build :order, product_ids: [product_1.id, product_2.id]
+    end
+
+    it 'returns the total amount to pay for the products' do
+      expect{@order.set_total!}.to change{@order.total}.from(0).to(185)
+    end
+  end
 end
